3da070af138e97ee09af7bc29b67045c82b960ba
d1iego7w7 <fofoletehurtado@gmial.com>
2022-09-25 20:36:07 -0400

    backend

--- backend/config/online.sql
+++ backend/config/online.sql
@@ -28,10 +28,11 @@ DeleteUserId int null
 
 create table Producto(
 Cod_producto int (11) primary key not null auto_increment,
-Nom_producto varchar (20) not null,
+Nom_producto varchar (50) not null,
 Precio_producto float not null,
 Descripcion TinyText not null,
 Fecha_lanzamiento date not null,
+PhotoGame varchar(100),
 Cod_categoria int not null,
 FOREIGN KEY (Cod_categoria) REFERENCES Categoria(Cod_categoria),
 CreationDate DateTime not null,
@@ -64,17 +65,25 @@ insert into  Usuario (Nombre_usuario,Apellido_Paterno,Apellido_Materno,Pais,Corr
 ('Juan Gustavo','Bravo','Ibañez','Bolivia','juan@gmail.com','2361de23','73340956', 1, now()),
 ('Wilfredo','Lopez','Perez','Bolivia','wilfredo@gmail.com','123456','73340956', 1, now());
 
-
 insert into Categoria (Nom_cateogoria,Deta_categoria,Publico, CreationDate)values 
 ('Terror','Este tipo de juegos contiene contenido fuerte','+18', now()),
-('Creativo','Este tipo de juegos contiene contenido para expresar creativida','+7', now()),
+('Accion','Este tipo de juegos contiene contenido violento','+18', now()),
+('Creativo','Este tipo de juegos contiene contenido para expresar creatividad','+7', now()),
+('Deporte','Este tipo de juegos contiene contenido es inspirado en deportes','+7', now()),
+('Arcade','Este tipo de juegos contiene contenido aventura para explorar','+12', now()),
+('Estrategia','Este tipo de juegos contiene contenido para estrategias','+18', now()),
 ('Shooter','Este tipo de juegos contiene contenido armas','+12',now());
 
-insert into Producto (Nom_producto,Precio_producto,Descripcion,Fecha_lanzamiento,Cod_categoria, CreationDate) values
-('Resident Evil V','134.99','Juego de terror','2022/08/12','1',now()),
-('Minecraft','69.99','Juego de creativor','2022/09/11','2',now()),
-('Fornite','89.99','Juego de disparos online','2022/07/25','3',now());
-
-
+insert into Producto (Nom_producto,Precio_producto,Descripcion,Fecha_lanzamiento,PhotoGame,Cod_categoria, CreationDate) values
+('Resident Evil V','134.99','Juego de terror','2022/08/12','resident.png','1',now()),
+('Minecraft','69.99','Juego de creativo construccion','2022/09/11','minecraft.jpeg','3',now()),
+('Uncharted 4','74.99','Juego de accion y aventuras','2022/09/01','unc.jpg','2',now()),
+('Fifa 22','59.99','Juego de futbol','2022/05/12','fifa.jpg','4',now()),
+('Crash Bandicoot','89.99','Juego de Arcade y diversion','2022/08/23','crash.jpg','5',now()),
+('Star craft','49.99','Juego de estrategia online','2022/06/30','star.jpg','6',now()),
+('Grand Theft Auto V','109.99','Juego de accion online','2022/06/21','gtav.jpg','2',now()),
+('League of Legends','75.99','Juego de estrategia de manera online','2022/04/19','lol.jpg','6',now()),
+('Counter Striker Global','24.99','Juego de shooter online','2022/09/11','apex.jpg','7',now()),
+('Fornite','89.99','Juego de disparos online','2022/07/25','fornite.jpg','7',now());
 
 
