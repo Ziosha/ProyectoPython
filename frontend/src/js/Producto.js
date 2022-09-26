33851248c45bb9c4af8f5ded94c3eed0888d7363
Ziosha <zioshadarkmetal@gmail.com>
2022-09-25 20:35:35 -0400

    style

---
+++ frontend/src/js/Producto.js
@@ -0,0 +1,7 @@
+let rootProduct =  document.getElementById('root'),
+    homProdruct = document.getElementById('home');
+
+homProdruct.addEventListener('click',()=> {
+        rootProduct.innerHTML = `<h1>Hola</h1>`
+})
+
